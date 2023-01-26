af7f5383e73b264dd2b3fd8f53fa8745175ccf82
Dmitry Shlagoff <richyannet@yandex.ru>
2023-01-26 12:30:34 +0100

    Add tracing layer + add custom headers to span

--- src/app/http.rs
+++ src/app/http.rs
@@ -1,13 +1,20 @@
 use std::sync::Arc;
 
 use axum::{
+    body::HttpBody,
     response::IntoResponse,
     routing::{get, post},
     Extension, Json, Router,
 };
-use http::Response;
+use http::{Method, Request, Response};
 use hyper::Body;
 use svc_utils::middleware::MeteredRoute;
+use tower_http::trace::TraceLayer;
+use tracing::{
+    error,
+    field::{self, Empty},
+    info, Span,
+};
 
 use super::{
     context::{AppContext, GlobalContext},
@@ -35,7 +42,50 @@ pub fn build_router(context: AppContext) -> Router {
 
     let routes = routes.merge(pingz_router);
 
-    routes.layer(svc_utils::middleware::LogLayer::new())
+    routes.layer(svc_utils::middleware::LogLayer::new()).layer(
+        TraceLayer::new_for_http()
+            .make_span_with(|request: &Request<Body>| {
+                let ulms_app_audience =
+                    request.headers().get("ulms-app-audience").map(field::debug);
+                let ulms_scope = request.headers().get("ulms-scope").map(field::debug);
+                let ulms_app_version = request.headers().get("ulms-app-version").map(field::debug);
+                let ulms_app_label = request.headers().get("ulms-app-label").map(field::debug);
+
+                let span = tracing::error_span!(
+                    "http-api-request",
+                    path = request.uri().path(),
+                    query = request.uri().query(),
+                    method = %request.method(),
+                    status_code = Empty,
+                    kind = Empty,
+                    detail = Empty,
+                    body_size = Empty,
+                    ulms_app_audience,
+                    ulms_app_label,
+                    ulms_app_version,
+                    ulms_scope,
+                );
+
+                if request.method() != Method::GET && request.method() != Method::OPTIONS {
+                    span.record(
+                        "body_size",
+                        &field::debug(request.body().size_hint().upper()),
+                    );
+                }
+
+                span
+            })
+            .on_response(
+                |response: &Response<_>, latency: std::time::Duration, span: &Span| {
+                    span.record("status_code", &field::debug(response.status()));
+                    if response.status().is_success() {
+                        info!("response generated in {:?}", latency);
+                    } else {
+                        error!("response generated in {:?}", latency);
+                    }
+                },
+            ),
+    )
 }
 
 impl IntoResponse for AppError {
