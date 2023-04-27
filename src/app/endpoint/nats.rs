7cf3729d6d5f56c6fce9579390b72d50571e19c3
Dmitry Shlagoff <richyannet@yandex.ru>
2023-04-27 11:35:06 +0200

    Fix publish rights

--- src/app/endpoint/nats.rs
+++ src/app/endpoint/nats.rs
@@ -62,7 +62,7 @@ fn build_token(
     let allowed_topic = format!("classrooms.{classroom_id}.unreliable");
     // agent.{account_id}.{request, response}
     let request_wildcard = format!("agent.*.request.{classroom_id}");
-    let response_wildcard = format!("agent.*.response.{classroom_id}");
+    let response_wildcard = format!("agent.*.response.{classroom_id}.*.*");
     let request_topic = format!("agent.{}.request.{classroom_id}", account_id.label());
     let response_topic = format!("agent.{}.response.{classroom_id}.*.*", account_id.label());
 
